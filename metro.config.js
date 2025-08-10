cf485c5d174ca3a2fe07d45d8a70750359bd2b9f
OmegasDev <157725661+OmegasDev@users.noreply.github.com>
2025-08-10 08:46:54 +0100

    Updated metro.config.js

--- metro.config.js
+++ metro.config.js
@@ -4,6 +4,8 @@ const config = getDefaultConfig(__dirname);
 
 // Add support for .wasm files
 config.resolver.assetExts.push('wasm');
-config.resolver.sourceExts.push('wasm');
+
+// Ensure .wasm files are treated as assets, not source files
+config.resolver.sourceExts = config.resolver.sourceExts.filter(ext => ext !== 'wasm');
 
 module.exports = config;
